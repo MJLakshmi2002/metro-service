1b25fccba9380ca26c8be112743b36d149c095a3
Lakshmi MJ <mjlakshmi66@gmail.com>
2025-02-15 13:48:24 +0530

    SOS  feature was implemented

--- metro/src/main/java/com/lakshmi/metro/controller/SOSController.java
+++ metro/src/main/java/com/lakshmi/metro/controller/SOSController.java
@@ -1,4 +1,21 @@
 package com.lakshmi.metro.controller;
+import com.lakshmi.metro.service.SOSService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/sos")
 
 public class SOSController {
+    @Autowired
+    private SOSService sosService;
+
+    @PostMapping
+    public String triggerSOS(@RequestParam String userId) {
+        return sosService.triggerSOS(userId);
+    }
 }
--- metro/src/main/java/com/lakshmi/metro/service/SOSService.java
+++ metro/src/main/java/com/lakshmi/metro/service/SOSService.java
@@ -1,4 +1,17 @@
 package com.lakshmi.metro.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SOSService {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public String triggerSOS(String userId) {
+        // Publish alert to Kafka
+        kafkaTemplate.send("sos-topic", "SOS Alert from user: " + userId);
+        return "SOS alert triggered";
+    }
 }
