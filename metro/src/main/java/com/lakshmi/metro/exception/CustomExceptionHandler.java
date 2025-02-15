f4b485227bec5ddd89c9b634cc6fcb6c1b2c8c19
Lakshmi MJ <mjlakshmi66@gmail.com>
2025-02-15 15:02:11 +0530

    custom exception handling

--- metro/src/main/java/com/lakshmi/metro/exception/CustomExceptionHandler.java
+++ metro/src/main/java/com/lakshmi/metro/exception/CustomExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.lakshmi.metro.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class CustomExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
