09a12f632fba26cd2ab8256bad2e18f2142a435c
Lakshmi MJ <mjlakshmi66@gmail.com>
2025-02-15 14:18:04 +0530

    Active User and config developed

--- metro/src/main/java/com/lakshmi/metro/controller/ActiveUsersController.java
+++ metro/src/main/java/com/lakshmi/metro/controller/ActiveUsersController.java
@@ -1,4 +1,23 @@
 package com.lakshmi.metro.controller;
 
+
+import com.lakshmi.metro.service.ActiveUsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/active-users")
+
 public class ActiveUsersController {
+
+    @Autowired
+    private ActiveUsersService activeUsersService;
+
+    @GetMapping
+    public String getActiveUsers() {
+        return activeUsersService.getActiveUsers();
+    }
 }
--- metro/src/main/java/com/lakshmi/metro/service/ActiveUsersService.java
+++ metro/src/main/java/com/lakshmi/metro/service/ActiveUsersService.java
@@ -1,4 +1,17 @@
 package com.lakshmi.metro.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ActiveUsersService {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    public String getActiveUsers() {
+        // Fetch active users from Redis
+        return redisTemplate.opsForValue().get("active-users");
+    }
 }
